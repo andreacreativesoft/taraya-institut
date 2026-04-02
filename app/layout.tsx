359679e46cdd790bbb959d8aab7fd2b3d1136742
AndreaCreativeSoftDesign <contact@andreacreative.ro>
2026-04-02 10:17:55 +0300

    Fix: remove explicit <head> from layout — caused React #418 hydration error wiping CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Quattrocento, Questrial } from "next/font/google";
+import Script from "next/script";
 import { getSettings } from "@/lib/settings";
 import "./globals.css";
 
@@ -34,29 +35,29 @@ export default async function RootLayout({
       lang="fr"
       className={`${quattrocento.variable} ${questrial.variable} antialiased`}
     >
-      <head>
+      <body>
         {gtmId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <>
+            <Script
+              id="gtm-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${gtmId}');`,
-            }}
-          />
-        )}
-      </head>
-      <body>
-        {gtmId && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
+              }}
             />
-          </noscript>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          </>
         )}
         {children}
       </body>
